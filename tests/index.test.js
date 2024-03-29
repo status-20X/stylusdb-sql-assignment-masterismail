eccd26c135f6a0581974838c8375a51683983d3e
Mohammed Ismail <mohammedismail523261@gmail.com>
2024-03-29 21:51:35 +0530

    tutorial step-04

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,12 +1,13 @@
 // tests/index.test.js
 
-const parseQuery = require('../src/queryParser');
+const executeSELECTQuery = require('../src/index');
 
-test('Parse SQL Query', () => {
+test('Execute SQL Query', async () => {
     const query = 'SELECT id, name FROM sample';
-    const parsed = parseQuery(query);
-    expect(parsed).toEqual({
-        fields: ['id', 'name'],
-        table: 'sample'
-    });
+    const result = await executeSELECTQuery(query);
+    expect(result.length).toBeGreaterThan(0);
+    expect(result[0]).toHaveProperty('id');
+    expect(result[0]).toHaveProperty('name');
+    expect(result[0]).not.toHaveProperty('age');
+    expect(result[0]).toEqual({ id: '1', name: 'John' });
 });
