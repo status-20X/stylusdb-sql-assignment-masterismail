b177842b470a28eaa6c032b2e7c6461af92e8287
Mohammed Ismail <mohammedismail523261@gmail.com>
2024-04-04 10:59:58 +0530

    debugges; all tests passing till here

--- src/queryParser.js
+++ src/queryParser.js
@@ -93,28 +93,6 @@ function parseQuery(query) {
         joinTable,
         joinCondition
     };
-
-    function parseJoinClause(query) {
-        const joinRegex = /\s(INNER|LEFT|RIGHT) JOIN\s(.+?)\sON\s([\w.]+)\s*=\s*([\w.]+)/i;
-        const joinMatch = query.match(joinRegex);
-    
-        if (joinMatch) {
-            return {
-                joinType: joinMatch[1].trim(),
-                joinTable: joinMatch[2].trim(),
-                joinCondition: {
-                    left: joinMatch[3].trim(),
-                    right: joinMatch[4].trim()
-                }
-            };
-        }
-    
-        return {
-            joinType: null,
-            joinTable: null,
-            joinCondition: null
-        };
-    }
 }
 
-module.exports = { parseQuery, parseJoinClause };
+module.exports = parseQuery;
